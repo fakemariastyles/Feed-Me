491d0467026cbaab250c40660d8e53edeacdbd8a
mary <fakemariastyles>
2019-05-22 14:15:37 +0430

    remote and data tested , works

--- app/src/main/java/com/mmb/feed_me/MainActivity.kt
+++ app/src/main/java/com/mmb/feed_me/MainActivity.kt
@@ -6,19 +6,24 @@ import android.util.AttributeSet
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.mmb.feed_me.data.repository.PostRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-    var postRepository: PostRepository? = null
+    @Inject lateinit var postRepository: PostRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        FeedMeApplication.component.inject(this)
-        println("got here")
-        val result = postRepository?.getPosts()
-            ?.subscribe({
-                println(it)
-            }, {
-                println(it)
-            })
+//        FeedMeApplication.component.inject(this)
+//        val result = postRepository?.getPosts()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            ?.subscribe({
+//                println(it)
+//            }, {
+//                println(it)
+//            })
+////        println(result)
     }
 }
--- app/src/main/java/com/mmb/feed_me/data/repository/PostRepository.kt
+++ app/src/main/java/com/mmb/feed_me/data/repository/PostRepository.kt
@@ -6,7 +6,7 @@ import com.mmb.feed_me.toPostEntity
 import io.reactivex.Observable
 import javax.inject.Inject
 
-class PostRepository @Inject constructor(val postDataSource: PostDataSource) {
+class PostRepository @Inject constructor(private val postDataSource: PostDataSource) {
     fun getPosts(): Observable<List<PostEntity>?> {
         return postDataSource.getPosts().map { items ->
             items.map { postDto ->
